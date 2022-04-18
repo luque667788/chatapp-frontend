ea74a3e5ef1bb0e9770dc529ccdeae554648239f
luque667788 <luquemendonca@gmail.com>
2022-04-18 11:14:25 -0300

    single chat room app working

--- src/App.vue
+++ src/App.vue
@@ -1,45 +1,25 @@
 <template>
   <div id="app">
-    <div class="container-fluid h-100">
-      <div class="row justify-content-center h-100">
-        <div class="col-md-8 col-xl-6 chat">
-          <div class="card">
-            <div class="card-header msg_head">
-              <div class="d-flex bd-highlight justify-content-center">Chat</div>
-            </div>
-            <div class="card-body msg_card_body">
-              <div
-                v-for="(message, key) in messages"
-                :key="key"
-                class="d-flex justify-content-start mb-4"
-              >
-                <div class="msg_cotainer">
-                  {{ message.body }}
-                  <span class="msg_time"></span>
-                </div>
-              </div>
-            </div>
-            <div class="card-footer">
-              <div class="input-group">
-                <textarea
-                  v-model="newMessage"
-                  name=""
-                  class="form-control type_msg"
-                  placeholder="Type your message..."
-                  v-on:keyup.enter.exact="sendMessage"
-                ></textarea>
-                <div class="input-group-append">
-                  <span
-                    class="input-group-text send_btn"
-                    v-on:click="sendMessage"
-                    >></span
-                  >
-                </div>
-              </div>
-            </div>
-          </div>
+    <div class="msg_head">
+      <div class="">Chat</div>
+    </div>
+    <div class="msgs-area">
+      <div v-for="(message, key) in messages" :key="key">
+        <div class="msg">
+          {{ message.body }}
+          <span class="msg_time"></span>
         </div>
       </div>
+    </div>
+    <div class="InputArea">
+      <textarea
+        v-model="newMessage"
+        name=""
+        class="textinput"
+        placeholder="Type your message..."
+        v-on:keyup.enter.exact="sendMessage"
+      ></textarea>
+      <span class="send_btn" v-on:click="sendMessage">></span>
     </div>
   </div>
 </template>
@@ -73,7 +53,7 @@ export default {
   mounted: function () {
     //runs when app starts
     console.log("Starting connection to WebSocket Server");
-    this.connection = new WebSocket("ws://localhost:8081/ws");
+    this.connection = new WebSocket("ws://localhost:3030/ws");
 
     this.connection.onmessage = (event) => {
       this.receiveMessage(event);
@@ -92,87 +72,54 @@ html {
   height: 100%;
   margin: 0;
   background: #0310ea;
-  background: -webkit-linear-gradient(to right, #560a86, #7122fa, #560a86);
-  background: linear-gradient(to right, #0a8621, #9894a0, #cc681b);
+  background: -webkit-linear-gradient(to right, #ffffff, #f7f7f7, #f5f5f5);
+  background: linear-gradient(to right, #ffffff, #ffffff, #ffffff);
 }
 
 #app {
   height: 100%;
 }
 
-.chat {
-  margin-top: auto;
-  margin-bottom: auto;
-}
-.card {
-  height: 500px;
-  border-radius: 15px;
-  background-color: rgba(0, 0, 0, 0.4);
-  border: 1px solid rgba(255, 255, 255, 0.2);
-}
-
 .msg_head {
-  color: #fff;
+  /*color: #fff;*/
   text-align: center;
   font-size: 26px;
 }
 
-.msg_card_body {
-  overflow-y: auto;
+.InputArea {
+  position: sticky;
+  top: 840px;
+  height: 70px;
 }
-.card-header {
-  border-radius: 15px 15px 0 0;
-  border-bottom: 0;
-}
-.card-footer {
-  border-radius: 0 0 15px 15px;
-  border-top: 0;
-}
-.container {
-  align-content: center;
-}
-
-.type_msg {
-  background-color: rgba(86, 10, 134, 0.6);
+.textinput {
+  font-size: 200%;
+  vertical-align: middle;
+  background-color: rgba(151, 151, 151, 0.6);
   border: 0;
+  width: 90%;
   color: white;
   height: 60px;
-  overflow-y: auto;
-}
-.type_msg:focus {
-  box-shadow: none;
-  outline: 0px;
-  background-color: rgba(255, 255, 255, 0.6);
+  margin-left: 10px;
+  border-radius: 10px 0px 0px 20px;
 }
 
 .send_btn {
-  border-radius: 0 15px 15px 0;
-  background-color: rgba(0, 0, 0, 0.3);
-  border: 0;
-  color: white;
+  margin-top: 100px;
+  margin-bottom: 100px;
+  text-align: center;
+  vertical-align: middle;
+  border-radius: 0px 10px 10px 0px;
+  padding: 21px;
+  background-color: rgba(27, 225, 27, 0.3);
+  color: rgb(17, 212, 30);
   cursor: pointer;
 }
 
-.msg_cotainer {
-  margin-top: auto;
-  margin-bottom: auto;
+.msg {
+  margin-top: 1%;
   margin-left: 10px;
-  border-radius: 25px;
+  border-radius: 20px 20px 20px 60px;
   background-color: #82ccdd;
   padding: 10px;
-  position: relative;
-}
-.msg_cotainer_send {
-  margin-top: auto;
-  margin-bottom: auto;
-  margin-right: 10px;
-  border-radius: 25px;
-  background-color: #75d5fd;
-  padding: 10px;
-  position: relative;
-}
-
-.msg_head {
-  position: relative;
 }
 </style>
