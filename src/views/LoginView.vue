2d9c1482b5053df68ee36d1d3ae90f0ec1a68629
luque667788 <luquemendonca@gmail.com>
2022-08-18 15:27:40 -0300

    okk

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -1,21 +1,26 @@
 <template>
   <div class="about">
     <h1>{{ this.userStore.username }}</h1>
-    <div class="bg-grey-lighter px-4 py-4 flex items-center">
+    <div class="bg-grey-lighter px-4 py-4 flex items-center justify-between">
       <input
         type="text"
-        class="w-full justify-start border rounded px-4 py-4"
-        placeholder="Insira sua mensagem"
+        class="border rounded px-4 py-4"
+        placeholder="USERNAME"
         v-model="this.username"
         v-on:keyup.enter="updateUsername"
       />
       <input
         type="text"
-        class="w-full justify-start border rounded px-4 py-4"
+        class="border rounded px-4 py-4"
         placeholder="PASSWORD"
         v-model="this.password"
         v-on:keyup.enter="updatePassword"
       />
+    </div>
+    <div class="flex items-center px-8 py-8" v-on:click="updateALL">
+      <div class="w-full border flex justify-center bg-slate-600 rounded-md">
+        <p class="p-2">Login/SignUp</p>
+      </div>
     </div>
   </div>
 </template>
